053adb42db3731d4c5262f5e0ad22ae62eab32ff
Susmita <92029064+suusmita@users.noreply.github.com>
2022-11-25 13:46:38 +0100

    Update AccountantController.java

--- src/AccountantController.java
+++ src/AccountantController.java
@@ -1,5 +1,19 @@
-public class AccountantController {
-    public void viewListOfDebts(){
+public class accountantController {
+    private boolean hasPaid = false;
+    private double expenses;
+
+    private int priceJunior = 1000;
+    private int priceSenior = 1600;
+
+    private int pensionerAge = 60;
+    private double pensionerDiscount = 0.75;
+    private double pricePensioner = 1600 * pensionerDiscount;
+    private int pricePassiveMember = 500;
+
+    private ChairmanController chairmanController;
+    private UserInterface userinterface;
+
+    public void setChairmanController(ChairmanController chairmanController) {
+        this.chairmanController = chairmanController;
 
     }
-}
