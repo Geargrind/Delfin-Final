28a027f5d04e5426b69f586a1c9554e741d52006
suusmita <susmidey2001@gmail.com>
2022-11-28 10:18:20 +0100

    Changed rename

--- src/AccountantController.java
+++ src/AccountantController.java
@@ -1,11 +1,10 @@
 public class AccountantController {
+
     private boolean hasPaid = false;
     private double expenses;
 
     private int priceJunior = 1000;
     private int priceSenior = 1600;
-
-    //Hejsa!
 
   
 
