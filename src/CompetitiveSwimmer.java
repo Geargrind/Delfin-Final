e2d5c7a6c37e7c48f58b38f25de8fa84e75bb805
hannan <hannanxnoor@gmail.com>
2022-11-28 11:48:13 +0100

    CompSwim

--- src/CompetitiveSwimmer.java
+++ src/CompetitiveSwimmer.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
+
 public class CompetitiveSwimmer extends Member{
+
+    private Enum diciplin;
+    private int time;
+
+    //constructor
+    public CompetitiveSwimmer (String name,int yearOfBirth,Enum diciplin, int time){
+        this.setName(name);
+        this.setYearOfBirth(yearOfBirth);
+        this.diciplin = diciplin;
+        this.time = time;
+
+    }
+
+    public static ArrayList<Member> butterfly(){
+        ArrayList<CompetitiveSwimmer> CompSwim1 = new ArrayList<>();
+        CompSwim1.add("")
+
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 
