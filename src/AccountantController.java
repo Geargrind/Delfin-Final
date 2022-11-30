7cf06e35a5e50cddc73d260f9a9664b4f0d23792
suusmita <susmidey2001@gmail.com>
2022-11-30 09:58:07 +0100

    Accountant class added metode

--- src/AccountantController.java
+++ src/AccountantController.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class AccountantController {
     private boolean hasPaid = false;
     private double expenses;
@@ -5,9 +7,48 @@ public class AccountantController {
     private int priceJunior = 1000;
     private int priceSenior = 1600;
 
+    private int pensionAge = 60;
+
+    public void accountantMenu{
+        boolean keepRunning = true;
+
+        while (true) {
+            switch (accountantMenu){
+                case 1;
 
 
-  
+                    public void payed() {
+                    Random rand = new Random();
 
- 
-    }
+                    for (int i = 0; i < accountantMenu.size(); i++) {
+                        int randNum = rand.nextInt((10) + 1);
+                        if (randNum < 2)
+                            hasPaid = false;
+                        else
+                            hasPaid = true;
+
+                        // todo metode for at se hvem der har betalt evt en setHasPaid(hasPaid); metode
+
+                    }
+                }
+
+
+                public void paymentChecker() {
+                    Random rand = new Random();
+
+                    for (int i = 0; i < accountantControllor.getAllMembers().size(); i++) {
+                        int randNum = rand.nextInt((10) + 1);
+                        if (randNum < 2)
+                            hasPaid = false;
+                        else
+                            hasPaid = true;
+
+                        //todo metode accountantMenu.getAllMembers().get(i).setHasPaid(hasPaid);
+
+                    }
+
+
+
+
+
+
