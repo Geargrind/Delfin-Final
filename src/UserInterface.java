113acf2a4ef094b6d0a9b32df1c3645af9918d2c
suusmita <susmidey2001@gmail.com>
2022-12-07 10:46:18 +0100

    top fem

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class UserInterface {
 
+
+    //method for top 5 ved ikke om den virker helt
+
+    public void displayTop5(ArrayList<CompetitiveSwimmer> disciplines, ArrayList<CompetitiveSwimmer> competitiveMembers) {
+        for (int i = 0; i < disciplines.size(); i++) {
+            for (int j = 0; j < 5; j++) {
+                if (disciplines.get(i) == competitiveMembers.get(j))
+                    System.out.println((i + 1) + "# " + disciplines.get(i).getName() + ", ID: " + disciplines.get(i).getMemberId() + ", Time: " + disciplines.get(i).getTime());
+            }
+
+        }
+
+    }
 }
